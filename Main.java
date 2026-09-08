ad4dd93e7364961c87b45916fc9b32a9cdc1fee6
bondarji <bondarji@gmail.com>
2026-09-08 14:08:57 +0300

    Новые изменения в person1

--- Main.java
+++ Main.java
@@ -6,13 +6,17 @@ public class Main {
 
 
         user1.setName("Вася");
-        user2.setName("Петя")
+        user2.setName("Петя");
 
         Message mes1 = new Message();
+        Message mes2 = new Message();
+
+        mes2.setInfo("ВАААААУ");
+
         mes1.setInfo("Хахахахахаха");
 
         System.out.println(user1.getName() + " написал сообщение: " + mes1.getInfo());
-        System.out.println(user2.getName() + " написал сообщение: " + mes1.getInfo());
+        System.out.println(user2.getName() + " написал сообщение: " + mes2.getInfo());
 
     }
 }
